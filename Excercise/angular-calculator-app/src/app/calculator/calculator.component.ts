437477a66ef3aed1bf10e366292c550dcc2079ff
Compass <ndphong165@gmail.com>
2022-11-17 18:08:48 +0700

    done calculator exercise

---
+++ Excercise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {Calculator} from '../calculator';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  calculator: Calculator = {};
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  getResult() {
+    switch (this.calculator.operator) {
+      case '+': {
+        // @ts-ignore
+        this.calculator.result = this.calculator.number1 + this.calculator.number2;
+        break;
+      }
+      case '-': {
+        // @ts-ignore
+        this.calculator.result = this.calculator.number1 - this.calculator.number2;
+        break;
+      }
+      case '*': {
+        // @ts-ignore
+        this.calculator.result = this.calculator.number1 * this.calculator.number2;
+        break;
+      }
+      case '/': {
+        // @ts-ignore
+        this.calculator.result = this.calculator.number1 / this.calculator.number2;
+        break;
+      }
+    }
+  }
+}
